4b21f82144bb5551bbdbb298ca7e06fdc0a2218d
Nishakalkandha <19bcs2048@gmail.com>
2026-06-09 12:24:29 +0530

    Deployment configuration added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@98rmagnajzipo6rhmk*wkw0nt3@biz8=p%2isho8i(koj!_!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
